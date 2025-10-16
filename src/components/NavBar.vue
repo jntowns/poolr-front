0c2ae377a1fb33559322dcec3d7c819fe4c4e6b7
insertusernamed <yevtushenkodan04@gmail.com>
2025-10-16 12:26:04 -0400

    removed starting files and added basic nav and pages

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/register">Register</router-link>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    background: #333;
+    color: white;
+    padding: 1em;
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+    margin: 0 1em;
+}
+
+nav a:hover {
+    text-decoration: underline;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { createPinia } from 'pinia'
+import router from './router'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.mount('#app')
