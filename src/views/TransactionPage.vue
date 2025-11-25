acebb4e3d6a8cefe85bcd3770423ca83bec76abf
aaron <girardaaron89@gmail.com>
2025-11-25 18:42:28 -0500

    updated translations

--- src/views/TransactionPage.vue
+++ src/views/TransactionPage.vue
@@ -2,7 +2,7 @@
   <div class="transaction-page">
     <div v-if="selectedRide" class="transaction-content">
       <section class="map-preview">
-        <h2>Route Map</h2>
+        <h2>{{ t("transaction_routeMap") }}</h2>
         <div class="map-wrapper">
           <RouteMap />
         </div>
@@ -10,9 +10,9 @@
 
       <section class="transaction-card">
         <header>
-          <h1>Confirm Your Ride</h1>
+          <h1>{{ t("transaction_confirmRide") }}</h1>
           <p class="provider-note">
-            Complete your booking with PayPal or a credit card.
+            {{ t("transaction_providerNote") }}
           </p>
         </header>
 
